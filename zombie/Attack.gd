f16495ccae7db6d3929543ad6c64d00de2e2f637
agustinoli <aolivaarias@gmail.com>
2022-06-11 03:06:38 -0300

    algunos cambios chiquitos

---
+++ zombie/Attack.gd
@@ -0,0 +1,10 @@
+extends State
+
+
+func physics_update(delta: float) -> void:
+	pass
+
+
+func _on_AttackArea_area_exited(area):
+	state_machine.transition_to('Idle')
+	print_debug('Paso a idle')
---
+++ zombie/Idle.gd
@@ -0,0 +1,12 @@
+extends State
+
+# Upon entering the state, we set the Player node's velocity to zero.
+func enter(_msg := {}) -> void:
+	pass
+
+func update(delta: float) -> void:
+	pass
+
+func _on_AttackArea_area_entered(area):
+	state_machine.transition_to('Attack')
+	print_debug('Paso a attack')
