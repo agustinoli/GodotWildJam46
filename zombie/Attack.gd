5c961e9485ca35eef537317fcd02ad0cc82ab498
Tomas Sarquis <tsarquis88@gmail.com>
2022-06-11 14:06:47 -0300

    discharge agregada

--- zombie/Attack.gd
+++ zombie/Attack.gd
@@ -4,7 +4,7 @@ extends State
 func physics_update(delta: float) -> void:
 	pass
 
-
 func _on_AttackArea_area_exited(area):
-	state_machine.transition_to('Idle')
-	print_debug('ZOMBIE: Paso a idle')
+	if area.get_name() == 'Orb':
+		state_machine.transition_to('Idle')
+		print_debug('ZOMBIE: Paso a idle')
