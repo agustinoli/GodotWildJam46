fc665f8edead80c6cd07cfddad293d2f3f57e90a
agustinoli <aolivaarias@gmail.com>
2022-06-12 16:59:50 -0300

    Feat: Player idle and run animations

--- player/Player.gd
+++ player/Player.gd
@@ -2,54 +2,30 @@ extends KinematicBody2D
 
 signal discharge
 
-var NORMAL_SPEED = 400
+var directions = ["Right", "RightDown", "Down", "LeftDown", "Left", "LeftUp", "Up", "RightUp"]
+var current_direction: String setget set_current_dir, get_current_dir
+var facing = Vector2() setget set_facing, get_facing
 
-var speed
-var velocity
-var direction
+func set_current_dir(new_dir: String):
+	current_direction = new_dir
+
+
+func get_current_dir() -> String:
+	return current_direction
+
+
+func get_dir(index: int) -> String:
+	return directions[index]
+
+
+func set_facing(new_facing: Vector2):
+	facing = new_facing
+
+
+func get_facing() -> Vector2:
+	return facing
+
 
 func _ready():
-	speed = NORMAL_SPEED
-	velocity = Vector2.ZERO
-	direction = 1
+	pass
 
-func _physics_process( delta ):
-	var collisioned_body
-	
-	parse_input()
-	collisioned_body = move_and_collide( delta * velocity )
-	
-	if collisioned_body != null:
-		print_debug( str( 'PLAYER: Colision con ', collisioned_body.collider.get_name() ) )
-
-func parse_input():
-	velocity = Vector2.ZERO
-	
-	if Input.is_action_pressed( "move_up" ):
-		velocity.y = -1
-	elif Input.is_action_pressed( "move_down" ):
-		velocity.y = 1
-	
-	if Input.is_action_pressed( "move_right" ):
-		velocity.x = 1
-	elif Input.is_action_pressed( "move_left" ):
-		velocity.x = -1
-	
-	if Input.is_action_pressed( "use_orb" ):
-		use_orb()
-	
-	if velocity.x == 1:
-		scale.x = scale.y * -1
-		direction = 1
-	elif velocity.x == -1:
-		scale.x = scale.y
-		direction = -1
-	
-	velocity = velocity.normalized() * speed
-
-func use_orb():
-	if $Orb.use():
-		emit_signal( 'discharge' )
-
-func get_direction():
-	return direction
