9b09541d79770ef58f22b6ceb64432917597a36b
agustinoli <aolivaarias@gmail.com>
2022-06-12 20:26:37 -0300

    Skeleton empezado

---
+++ utils/AnimationResMaker.py
@@ -0,0 +1,21 @@
+
+
+
+directions = ["Left", "LeftUp", "Up", "RightUp","Right", "RightDown", "Down", "LeftDown"]
+
+animations = ["Idle","Run","Attack","Doe","Alt"]
+
+anim_size = [4,8,8,8,4]
+
+"animations = [ {
+"frames": [  ],
+"loop": true,
+"name": "New Anim",
+"speed": 5.0
+},"
+
+for dir in directions:
+
+    for anim in animations:
+
+        
