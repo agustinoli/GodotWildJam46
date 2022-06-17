a847e4d59e14b5c4d246b7c2a15691308d542cfa
agustinoli <aolivaarias@gmail.com>
2022-06-17 15:22:13 -0300

    Fix: discharge working again

--- Main.gd
+++ Main.gd
@@ -12,7 +12,7 @@ func _process( _delta ):
 func _on_Player_discharge():
 	var discharge_node = discharge_scene.instance()
 	add_child( discharge_node )
-	discharge_node.init( $Player.get_position(), $Player.get_current_dir() )
+	discharge_node.init( $YSort/Player.get_position(), $YSort/Player.get_current_dir() )
 
 func _on_Player_orb_selected(orb_type):
 	$HUD.set_current_orb(orb_type)
